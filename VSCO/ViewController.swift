dfd8c69034fce82cd8bc426a515cd696c57ba53c
AnnaSemichaeva <tdhnss@gmail.com>
2022-03-27 23:48:39 +0300

    minor changes

--- VSCO/ViewController.swift
+++ VSCO/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var picture: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func editAction(_ sender: Any) {
+    }
     
     @IBAction func addAction(_ sender: Any) {
     }
