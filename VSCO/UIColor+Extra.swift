181fc974121fe2108e035c6aa2f92f306a25138e
AnnaSemichaeva <tdhnss@gmail.com>
2022-03-30 23:36:45 +0300

    Update UIColor+Extra.swift

--- VSCO/UIColor+Extra.swift
+++ VSCO/UIColor+Extra.swift
@@ -8,4 +8,9 @@
 import Foundation
 import UIKit
 
-extension UIColor
+extension UIColor {
+    
+    struct Filter {
+        static let yellow = UIColor(red: 0.908, green: 0.883, blue: 0.655, alpha: 0.2)
+    }
+}
